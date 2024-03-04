324d31d6b13a0d8fa45709f975762d7a62107b7c
IlyushaShulenin <ilya.shulenin36@gmail.com>
2024-03-04 16:48:35 +0300

    Подключена база данных
    * добавлена зависимость liquibase
    * прописан changelog-master
    * создан changelog-1.0

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,45 @@
+--liquibase formatted sql
+
+--changeset ilya_shulenin:1
+CREATE TABLE worker(
+    id BIGSERIAL,
+    email VARCHAR(128) UNIQUE,
+    name VARCHAR(64),
+    surname VARCHAR(64),
+    status VARCHAR(32),
+
+    PRIMARY KEY (id)
+);
+
+--changeset ilya_shulenin:2
+CREATE TABLE product(
+    id BIGSERIAL,
+    name VARCHAR(128) UNIQUE,
+    measure VARCHAR(32),
+
+    PRIMARY KEY (id)
+);
+
+--changeset ilya_shulenin:3
+CREATE TABLE plan(
+    id BIGSERIAL,
+    worker_id BIGINT,
+    product_id BIGINT,
+    amount FLOAT,
+    date DATE,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (worker_id) REFERENCES worker (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+----changeset ilya_shulenin:4
+CREATE TABLE score(
+    id BIGSERIAL,
+    worker_id BIGINT,
+    score SMALLINT,
+    plan_is_completed BOOLEAN,
+    date DATE,
+
+    PRIMARY KEY (id)
+);
