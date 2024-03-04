9a2b5d2c2e4ab47ae8d3ef11da21d0addb79b86c
IlyushaShulenin <ilya.shulenin36@gmail.com>
2024-03-04 16:09:58 +0300

    Empty project

---
+++ src/main/java/ru/shulenin/farmservice/FarmServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.shulenin.farmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmServiceApplication.class, args);
+    }
+
+}
